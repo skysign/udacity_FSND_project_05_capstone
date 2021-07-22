a6b8de357c50595ec03f632d5620608dd763c603
ByeongKeon Kim <skysign@gmail.com>
2021-07-22 11:12:30 +0900

    Implemented GET/POST for Actor

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request, abort, jsonify
 from flask_cors import CORS
 from models import setup_db
 
@@ -7,6 +7,12 @@ def create_app(test_config=None):
     app = Flask(__name__)
     setup_db(app)
     CORS(app)
+
+    def check(dt):
+        if dt is None:
+            return False
+
+        return True
 
     @app.route('/')
     def get_greeting():
@@ -20,10 +26,76 @@ def create_app(test_config=None):
     def be_cool():
         return "Be cool, man, be coooool! You're almost a FSND grad!"
 
+    @app.route('/actors', methods=['GET'])
+    def get_actors():
+        actors = None
+
+        try:
+            actors = Actors.query.all()
+        except Exception as e:
+            print(e)
+            abort(404)
+
+        if check(drinks) is False:
+            abort(404)
+
+        ds = [ac.get_as_json() for ac in actors]
+
+        return jsonify({
+            'success': True,
+            'actors': ds
+        })
+
+    @app.route('/actors/<int:id>', methods=['GET'])
+    def get_actor_id(id):
+        actor = None
+
+        try:
+            actor = Actors.query.filter_by(id=id).first()
+        except Exception as e:
+            print(e)
+            abort(404)
+
+        if check(actor) is False:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'actor': actor.get_as_json()
+        })
+
+    @app.route('/actors', methods=['POST'])
+    def post_actors():
+        post_json = request.get_json()
+
+        name = post_json.get('name')
+        age = post_json.get('age')
+        gender = post_json.get('gender')
+        description = post_json.get('description')
+
+        if ((name is None)
+            or (age is None)
+            or (gender is None)
+            or (description is None)):
+            abort(422)
+
+        try:
+            actor = Actors(
+                name, age, gender, description=description)
+            actor.insert()
+
+        except Exception as e:
+            print(e)
+            abort(422)
+
+        return jsonify({
+            'success': True,
+            'actor_id': actor_id
+        })
+
     return app
 
 application = create_app()
 
 if __name__ == '__main__':
     application.run()
-
--- models.py
+++ models.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_path = os.environ['DATABASE_URL']
-
 db = SQLAlchemy()
 
 '''
@@ -18,25 +17,91 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
-
 '''
 Person
 Have title and release year
 '''
 class Person(db.Model):  
-  __tablename__ = 'People'
+    __tablename__ = 'People'
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
-  catchphrase = Column(String)
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    catchphrase = Column(String)
 
-  def __init__(self, name, catchphrase=""):
-    self.name = name
-    self.catchphrase = catchphrase
+    def __init__(self, name, catchphrase=""):
+        self.name = name
+        self.catchphrase = catchphrase
 
-  def format(self):
-    return {
-      'id': self.id,
-      'name': self.name,
-      'catchphrase': self.catchphrase}
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'catchphrase': self.catchphrase}
 
+class Actor(db.Model):
+    __tablename__ = 'actors'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    age = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.String(), nullable=False)
+    description = db.Column(db.String(), nullable=True, default='')
+
+    def __init__(self, name, age, gender, description=""):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.description = description
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_as_json(self):
+        return({
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            "description": self.description,
+        })
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    released_date = db.Column(db.Date(), nullable=False)
+    genre = db.Column(db.String(), nullable=True, default='')
+    description = db.Column(db.String(), nullable=True, default='')
+
+    def __init__(self, title, released_date, genre, description=""):
+        self.title = title
+        self.released_date = released_date
+        self.genre = genre
+        self.description = description
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def get_as_json(self):
+        return({
+            "id": self.id,
+            "title": self.title,
+            "released_date": self.released_date,
+            "genre": self.genre,
+            "description": self.description,
+        })
